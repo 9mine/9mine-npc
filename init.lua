7a3b7dadc2d9ae2baee8c72b1e3e25750b543aea
Oleksandr Dievri <o.dievri@gmail.com>
2020-09-25 00:18:52 +0300

    fix path

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ data = require 'data'
 socket = require 'socket'
 pprint = require 'pprint'
 readdir = require 'readdir'
-local path = minetest.get_modpath("9mine-npc")
+local path = minetest.get_modpath("9mine_npc")
 dofile(path .. "/config.lua")
 dofile(path .. "/on_player.lua")
 dofile(path .. "/spawn.lua")
